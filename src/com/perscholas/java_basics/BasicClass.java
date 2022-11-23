227e8f9c33815a5c62a6294d2882988af705df6b
Edward Barcenas <edward.barcenas16@gmail.com>
2022-11-23 00:01:51 -0600

    wrote the first 3 programs

--- src/com/perscholas/java_basics/BasicClass.java
+++ src/com/perscholas/java_basics/BasicClass.java
@@ -4,7 +4,25 @@ public class BasicClass {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
 	}
+	
+	public void sumInt(int a, int b) {
+		int sum = a + b;
+		System.out.println(sum);
+	}
+	
+	public void sumDouble(double a, double b) {
+		double sum = a + b;
+		System.out.println(sum);
+	}
+	
+	public void sum(int a, double b) {
+		double sum = a + b;
+		System.out.println(sum);
+	}
+	
+	
+	
 
 }
