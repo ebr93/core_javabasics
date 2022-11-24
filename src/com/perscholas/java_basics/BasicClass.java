999aba3d8daf298b36489d5368aa77f8a75563e9
Edward Barcenas <edward.barcenas16@gmail.com>
2022-11-23 23:49:10 -0600

    added 2 more methods, division related

--- src/com/perscholas/java_basics/BasicClass.java
+++ src/com/perscholas/java_basics/BasicClass.java
@@ -4,25 +4,45 @@ public class BasicClass {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		int a = 12;
+		int b = 15;
 		
+		double A = 15;
+		double B = 5.5;
+		
+		sumInt(a, b);
+		sumDouble(A, B);
+		sum(a, B);
+		division(a, b);
 	}
 	
-	public void sumInt(int a, int b) {
+	public static void sumInt(int a, int b) {
 		int sum = a + b;
 		System.out.println(sum);
 	}
 	
-	public void sumDouble(double a, double b) {
+	public static void sumDouble(double a, double b) {
 		double sum = a + b;
 		System.out.println(sum);
 	}
 	
-	public void sum(int a, double b) {
+	public static void sum(int a, double b) {
 		double sum = a + b;
 		System.out.println(sum);
 	}
 	
+	public static void division(int a, int b) {
+		double quotient = a > b ? (double)a / b 
+				: (double)b / a;
+		System.out.println(quotient);
+	}
 	
+	public static void divisionDouble(double a, double b) {
+		double quotient = a > b ? a / b : b / a;
+		System.out.println(quotient);
+		int intQ = (int)quotient;
+		System.out.println(quotient);
+	}
 	
 
 }
